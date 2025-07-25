23b2878d4086b0f3023bbb82047ae0c6b5bf71fd
Miguel Barroso <miguelreisbarroso@gmail.com>
2025-07-25 21:21:07 +0100

    add Worker entity and update PaymentService to retrieve worker data from hr-worker microservice

--- hr-payroll/src/main/java/com/miguelbarroso/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/miguelbarroso/hrpayroll/services/PaymentService.java
@@ -1,11 +1,29 @@
 package com.miguelbarroso.hrpayroll.services;
 
 import com.miguelbarroso.hrpayroll.entities.Payment;
+import com.miguelbarroso.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.00, days);
+        Map<String , String> uriVars = new HashMap<>();
+        uriVars.put("id", ""+workerId);
+        Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVars);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
