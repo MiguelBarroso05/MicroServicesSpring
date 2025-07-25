e1777548c9a1efaec558a8f064892a8bcfc08321
Miguel Barroso <miguelreisbarroso@gmail.com>
2025-07-25 22:12:39 +0100

    add Ribbon client configuration and update WorkerFeignClient to use service discovery

--- hr-worker/src/main/java/com/miguelbarroso/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/miguelbarroso/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.miguelbarroso.hrworker.resources;
 
 import com.miguelbarroso.hrworker.entities.Worker;
 import com.miguelbarroso.hrworker.repository.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,23 +13,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
     @GetMapping
-    public ResponseEntity<List<Worker>> findAll(){
+    public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = repository.findAll();
         return ResponseEntity.ok().body(list);
     }
+
     @GetMapping("{id}")
-    public ResponseEntity<Worker> findByID(@PathVariable Long id){
-        Optional<Worker>  worker = repository.findById(id);
-        return ResponseEntity.ok().body(worker.get());
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
+        Worker worker = repository.findById(id).get();
+        return ResponseEntity.ok().body(worker);
     }
 }
